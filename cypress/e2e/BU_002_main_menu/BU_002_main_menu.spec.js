871cc1055ceba65e3ed02b5cbca2111ee1693b78
abuzova <alla.abuzova@gmail.com>
2023-03-08 09:01:01 +0100

    updating cucumber settings

--- cypress/e2e/BU_002_main_menu/BU_002_main_menu.spec.js
+++ cypress/e2e/BU_002_main_menu/BU_002_main_menu.spec.js
@@ -2,6 +2,6 @@ import { When } from "cypress-cucumber-preprocessor/steps";
 
 const url = "https://github.com";
 
-When(`I open GitHub page`, () => {
+When('I open GitHub page', () => {
   cy.visit(url);
 });
