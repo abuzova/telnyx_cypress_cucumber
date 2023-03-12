03fc6b23bc5c0e4933e34577f45991321889d35e
abuzova <alla.abuzova@gmail.com>
2023-03-12 01:23:06 +0100

    version 1.0 BU_006_Download_SIP_Trunking_pricing

---
+++ cypress/e2e/pages/SIPTrunkingPricingPage.spec.js
@@ -0,0 +1,49 @@
+class SIPTrunkingPricingPage {
+    
+    getFirstNameInput(){
+        return cy.get('div:nth-child(2) form input[id="FirstName"]');
+    }
+
+    getLastNameInput(){
+        return cy.get('div:nth-child(2) form input[id="LastName"]');
+    }
+
+    getEmailInput(){
+        return cy.get('div:nth-child(2) form input[id="Email"]');
+    }
+
+    getSubscriptionCheckbox(){
+        return cy.get('div:nth-child(2) form input[name="Subscription_Opt_In__c"][type="checkbox"]');
+    }
+
+    getSubmitButton(){
+        return cy.get('div:nth-child(2) form button');
+    }
+
+    setFirstNameInput(firstNameValue){
+        this.getFirstNameInput().type(firstNameValue);        
+        this.getFirstNameInput().should('have.value', firstNameValue)
+    }
+
+    setLastNameInput(lastNameValue){
+        this.getLastNameInput().type(lastNameValue);
+        this.getLastNameInput().should('have.value', lastNameValue);
+    }
+
+    setEmailInput(emailValue){
+        this.getEmailInput().type(emailValue);
+        this.getEmailInput().should('have.value', emailValue);
+    }
+
+    checkSubscriptionCheckbox(){      
+        this.getSubscriptionCheckbox()
+            .check()
+            .should('be.checked');
+    }
+
+    clickSubmitButton(){
+        this.getSubmitButton().click();
+    }
+}
+    const sip_trunking_pricing_page = new SIPTrunkingPricingPage();
+    export default sip_trunking_pricing_page;
